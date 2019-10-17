5d342a83da158a61fa74944d7d7058fc4d886880
Jad2Wizard <526654076@qq.com>
2019-10-17 21:17:09 +0800

    M: 使用 async/await 控制异步加载模型

--- src/Pages/Manage/ModelCard.js
+++ src/Pages/Manage/ModelCard.js
@@ -1,49 +1,50 @@
 import React from 'react'
-import { Row, Col, Icon } from 'antd'
+import {Row, Col, Icon} from 'antd'
 import moment from 'moment'
 import styles from './ModelCard.less'
 
-const ModelCard = React.memo(({ model, onSelect, onDelete }) => {
-    const { name, image, createTime, updateTime } = model
+const ModelCard = React.memo(({model, onSelect, onDelete}) => {
+	const {name, image, createTime, updateTime} = model
 
-    return (
-        <div className={styles.container} onClick={() => onSelect(model.id)}>
-            <img src={image || '/img/timg.c98df3e.jpg'} />
-            <div className={styles.body}>
-                <Row key="name">
-                    <Col key="title" span={8}>
-                        模型名称：
-                    </Col>
-                    <Col key="value" span={16}>
-                        {name}
-                    </Col>
-                </Row>
-                <Row key="createTime">
-                    <Col key="title" span={8}>
-                        创建时间：
-                    </Col>
-                    <Col key="value" span={16}>
-                        {moment(createTime).format('YYYY-MM-DD HH:mm:ss')}
-                    </Col>
-                </Row>
-                <Row key="updateTime">
-                    <Col key="title" span={8}>
-                        更新时间：
-                    </Col>
-                    <Col key="value" span={16}>
-                        {moment(updateTime).format('YYYY-MM-DD HH:mm:ss')}
-                    </Col>
-                </Row>
-            </div>
-            <Icon
-                type="delete"
-                onClick={e => {
-                    e.stopPropagation()
-                    onDelete(model.id)
-                }}
-            />
-        </div>
-    )
+	return (
+		<div className={styles.container} onClick={() => onSelect(model.id)}>
+			<img src={image || '/img/timg.c98df3e.jpg'} />
+			<div className={styles.body}>
+				<Row key="name">
+					<Col key="title" span={8}>
+						模型名称：
+					</Col>
+					<Col key="value" span={16}>
+						{name}
+					</Col>
+				</Row>
+				<Row key="createTime">
+					<Col key="title" span={8}>
+						创建时间：
+					</Col>
+					<Col key="value" span={16}>
+						{moment(createTime).format('YYYY-MM-DD HH:mm:ss')}
+					</Col>
+				</Row>
+				<Row key="updateTime">
+					<Col key="title" span={8}>
+						更新时间：
+					</Col>
+					<Col key="value" span={16}>
+						{moment(updateTime).format('YYYY-MM-DD HH:mm:ss')}
+					</Col>
+				</Row>
+			</div>
+			<Icon
+				type="delete"
+				className={styles.delete}
+				onClick={e => {
+					e.stopPropagation()
+					onDelete(model.id)
+				}}
+			/>
+		</div>
+	)
 })
 
 export default ModelCard
--- src/Pages/Show/index.js
+++ src/Pages/Show/index.js
@@ -1,174 +1,200 @@
-import React, { useEffect, useState, useCallback, useRef } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React, {useEffect, useState, useCallback, useRef} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'
+import {MTLLoader} from 'three/examples/jsm/loaders/MTLLoader'
 import elementResizeEvent from 'element-resize-event'
-import { getModel } from './../Manage/actions'
+import {getModel} from './../Manage/actions'
 import styles from './index.less'
 
 window.THREE = THREE
 const ObjLoader = new OBJLoader()
 const MtlLoader = new MTLLoader()
+const defaultModelColor = '#888'
 const zeroPoint = new THREE.Vector3(0, 0, 0)
+
+const loadObj = url =>
+	new Promise((resolve, reject) => {
+		ObjLoader.load(url, obj => resolve(obj), () => {}, err => reject(err))
+	})
+
+const loadMtl = url =>
+	new Promise((resolve, reject) => {
+		MtlLoader.load(url, mtl => resolve(mtl), () => {}, err => reject(err))
+	})
+
 const delay = timeout =>
-    new Promise(res => {
-        setTimeout(res, timeout)
-    })
+	new Promise(res => {
+		setTimeout(res, timeout)
+	})
 
 const Show = React.memo(() => {
-    const dispatch = useDispatch()
+	const dispatch = useDispatch()
 
-    const [model, setModel] = useState(null)
-    const modelId = location.search.split('id=')[1]
-    const modelList = useSelector(state => state.manageState.modelList)
+	const [model, setModel] = useState(null)
+	const modelId = location.search.split('id=')[1]
+	const modelList = useSelector(state => state.manageState.modelList)
 
-    useEffect(() => {
-        if (!modelId) return
-        const model = modelList.find(m => m.id === modelId)
-        if (!model) {
-            dispatch(getModel('request', { id: modelId }))
-        } else {
-            setModel(model)
-        }
-    }, [modelList])
+	useEffect(() => {
+		if (!modelId) return
+		const model = modelList.find(m => m.id === modelId)
+		if (!model) {
+			dispatch(getModel('request', {id: modelId}))
+		} else {
+			setModel(model)
+		}
+	}, [modelList])
 
-    const [container, setContainer] = useState(null)
+	const [container, setContainer] = useState(null)
 
-    const renderer = useRef(null)
-    const scene = useRef(null)
-    const camera = useRef(null)
-    const control = useRef(null)
-    const center = useRef(zeroPoint)
-    const radius = useRef(0)
-    const pointLight = useRef(null)
+	const renderer = useRef(null)
+	const scene = useRef(null)
+	const camera = useRef(null)
+	const control = useRef(null)
+	const center = useRef(zeroPoint)
+	const radius = useRef(0)
+	const pointLight = useRef(null)
+	const object = useRef(null)
 
-    const containerRef = useCallback(node => {
-        if (node) setContainer(node)
-    }, [])
+	const containerRef = useCallback(node => {
+		if (node) setContainer(node)
+	}, [])
 
-    useEffect(() => {
-        if (!container || !model) return
+	useEffect(() => {
+		async function init() {
+			if (!container || !model) return
 
-        const { clientWidth, clientHeight } = container
+			const {clientWidth, clientHeight} = container
 
-        scene.current = new THREE.Scene()
+			scene.current = new THREE.Scene()
 
-        camera.current = new THREE.PerspectiveCamera(
-            45,
-            clientWidth / clientHeight,
-            1,
-            200000
-        )
-        // camera.current.position.x = 12620
-        // camera.current.position.y = 64238
-        // camera.current.position.z = 65242
+			camera.current = new THREE.PerspectiveCamera(
+				45,
+				clientWidth / clientHeight,
+				1,
+				200000
+			)
 
-        window.camera = camera.current
-        const ambientLight = new THREE.AmbientLight(0xcccccc, 0.4)
-        scene.current.add(ambientLight)
-        pointLight.current = new THREE.PointLight(0xffffff, 0.8)
-        camera.current.add(pointLight.current)
-        scene.current.add(camera.current)
+			window.scene = scene.current
+			window.camera = camera.current
 
-        renderer.current = new THREE.WebGLRenderer({
-            antialias: true
-        })
-        renderer.current.setClearColor(0xffffff, 1.0)
-        renderer.current.setPixelRatio(window.devicePixelRatio)
-        renderer.current.setSize(clientWidth, clientHeight)
-        renderer.current.autoClear = true
+			renderer.current = new THREE.WebGLRenderer({
+				antialias: true
+			})
+			renderer.current.setClearColor(0xffffff, 1.0)
+			renderer.current.setPixelRatio(window.devicePixelRatio)
+			renderer.current.setSize(clientWidth, clientHeight)
+			renderer.current.autoClear = true
 
-        container.appendChild(renderer.current.domElement)
+			container.appendChild(renderer.current.domElement)
 
-        elementResizeEvent(container, handleResize)
+			elementResizeEvent(container, handleResize)
 
-        animate()
+			animate()
 
-        if (model.mtlFile) {
-            MtlLoader.load(model.mtlFile, materials => {
-                materials.preload()
-                ObjLoader.setMaterials(materials).load(
-                    model.objFile,
-                    object => {
-                        scene.current.add(object)
-                        window.object = object
-                        calcObjCenter(object)
-                    }
-                )
-            })
-        } else {
-            ObjLoader.load(model.objFile, object => {
-                scene.current.add(object)
-                object.current = object
-                calcObjCenter(object)
-            })
-        }
-    }, [container, model])
+			if (model.mtlFile) {
+				const material = await loadMtl(model.mtlFile)
+				material.preload()
+				ObjLoader.setMaterials(material)
+			}
 
-    const handleResize = useCallback(() => {
-        const { clientWidth, clientHeight } = container
+			const modelObj = await loadObj(model.objFile)
+			for (let c of modelObj.children) {
+				const {r, g, b} = c.material.color
+				if (r + g + b === 0) {
+					c.material.color = new THREE.Color(defaultModelColor)
+				}
+			}
 
-        camera.current.aspect = clientWidth / clientHeight
-        camera.current.updateProjectionMatrix()
+			scene.current.add(modelObj)
 
-        renderer.current.setSize(clientWidth, clientHeight)
-    }, [container])
+			window.object = modelObj
+			object.current = modelObj
 
-    const animate = useCallback(() => {
-        requestAnimationFrame(animate)
-        render()
-    }, [])
+			calcObjCenter(modelObj)
+			initLight()
+		}
 
-    const render = useCallback(() => {
-        camera.current.lookAt(center.current)
-        if (control.current) control.current.update()
-        renderer.current.render(scene.current, camera.current)
-    }, [])
+		init()
+	}, [container, model])
 
-    const calcObjCenter = useCallback(async object => {
-        const newCenter = new THREE.Vector3(0, 0, 0)
-        let newRadius = 0
-        for (let child of object.children) {
-            let boundingSphere = child.geometry.boundingSphere
-            while (!boundingSphere) {
-                await delay(200)
-                boundingSphere = child.geometry.boundingSphere
-            }
-            const { x, y, z } = boundingSphere.center
-            newCenter.x += x
-            newCenter.y += y
-            newCenter.z += z
-            const temp = Math.max(x, y, z)
-            if (temp > newRadius) newRadius = temp
-        }
-        const meshNum = object.children.length
-        if (meshNum > 0) {
-            newCenter.x = newCenter.x / meshNum
-            newCenter.y = newCenter.y / meshNum
-            newCenter.z = newCenter.z / meshNum
-        }
+	const initLight = useCallback(() => {
+		const ambientLight = new THREE.AmbientLight(0xcccccc, 0.4)
+		scene.current.add(ambientLight)
 
-        center.current = newCenter
-        radius.current = newRadius
+		pointLight.current = new THREE.PointLight(0xffffff, 0.8)
+		camera.current.add(pointLight.current)
+		scene.current.add(camera.current)
 
-        control.current = new OrbitControls(
-            camera.current,
-            renderer.current.domElement,
-            newCenter
-        )
+		const dLight = new THREE.DirectionalLight('#fff')
+		dLight.target = object.current
+		dLight.position.set(1, 1, 1)
+		// scene.current.add(dLight)
+	}, [])
 
-        camera.current.position.x = (1.6 * newRadius) / 1.73205
-        camera.current.position.y = (2.0 * newRadius) / 1.73205 //除以根号3
-        camera.current.position.z = (1.6 * newRadius) / 1.73205
+	const handleResize = useCallback(() => {
+		const {clientWidth, clientHeight} = container
 
-        pointLight.current.position.x = 1 * newRadius
-        pointLight.current.position.y = 1 * newRadius
-        pointLight.current.position.z = 1 * newRadius
-    }, [])
+		camera.current.aspect = clientWidth / clientHeight
+		camera.current.updateProjectionMatrix()
 
-    return <div ref={containerRef} className={styles.container}></div>
+		renderer.current.setSize(clientWidth, clientHeight)
+	}, [container])
+
+	const animate = useCallback(() => {
+		requestAnimationFrame(animate)
+		render()
+	}, [])
+
+	const render = useCallback(() => {
+		camera.current.lookAt(center.current)
+		if (control.current) control.current.update()
+		renderer.current.render(scene.current, camera.current)
+	}, [])
+
+	const calcObjCenter = useCallback(async object => {
+		const newCenter = new THREE.Vector3(0, 0, 0)
+		let newRadius = 0
+		for (let child of object.children) {
+			let boundingSphere = child.geometry.boundingSphere
+			while (!boundingSphere) {
+				await delay(200)
+				boundingSphere = child.geometry.boundingSphere
+			}
+			const {x, y, z} = boundingSphere.center
+			newCenter.x += x
+			newCenter.y += y
+			newCenter.z += z
+			const temp = Math.max(x, y, z)
+			if (temp > newRadius) newRadius = temp
+		}
+		const meshNum = object.children.length
+		if (meshNum > 0) {
+			newCenter.x = newCenter.x / meshNum
+			newCenter.y = newCenter.y / meshNum
+			newCenter.z = newCenter.z / meshNum
+		}
+
+		center.current = newCenter
+		radius.current = newRadius
+
+		control.current = new OrbitControls(
+			camera.current,
+			renderer.current.domElement,
+			newCenter
+		)
+
+		camera.current.position.x = (1.6 * newRadius) / 1.73205
+		camera.current.position.y = (2.0 * newRadius) / 1.73205 //除以根号3
+		camera.current.position.z = (1.6 * newRadius) / 1.73205
+
+		pointLight.current.position.x = 1 * newRadius
+		pointLight.current.position.y = 1 * newRadius
+		pointLight.current.position.z = 1 * newRadius
+	}, [])
+
+	return <div ref={containerRef} className={styles.container}></div>
 })
 
 export default Show
